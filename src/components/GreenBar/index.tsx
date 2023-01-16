912a7449785e897cd1518f96776358585ee1fdbd
antonm7 <migolkoanton@gmail.com>
2023-01-16 12:14:05 +0200

    green bar styled

--- src/components/GreenBar/index.tsx
+++ src/components/GreenBar/index.tsx
@@ -1,9 +1,33 @@
 import styles from './index.module.scss'
+import one from '/greenBar/one.png'
+import two from '/greenBar/two.png'
+import three from '/greenBar/three.png'
+import four from '/greenBar/four.png'
 
 export default function GreenBar() {
     return (
-        <div id={styles.wrapper} className=" bg-greenBg">
-            GreeBar
+        <div id={styles.wrapper} className="py-9 flex justify-center bg-greenBg ">
+            <div className='px-16 py-3 text-center flex flex-col items-center'>
+                <img src={one} className="h-24"/>
+                <h3 className='font-main text-5xl font-black py-2'>350K</h3>
+                <p className='font-secondary text-lg text-grayText'>plant species</p>
+            </div>
+            <div className='px-16 py-3 text-center flex flex-col items-center'>
+                <img src={two} className="h-24"/>
+                <h3 className='font-main text-5xl font-black py-2'>10k+</h3>
+                <p className='font-secondary text-lg text-grayText'>plant recognations</p>
+            </div>
+            <div className='px-16 py-3 text-center flex flex-col items-center'>
+                <img src={three} className="h-24"/>
+                <h3 className='font-main text-5xl font-black py-2'>700+</h3>
+                <p className='font-secondary text-lg text-grayText'>insect recognition</p>
+            </div>
+            <div className='px-16 py-3 text-center flex flex-col items-center'>
+                <img src={four} className="h-24"/>
+                <h3 className='font-main text-5xl font-black py-2'>950k+</h3>
+                <p className='font-secondary text-lg text-grayText'>saved plants</p>
+            </div>
+
         </div>
     )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,22 @@ import two from '/header/two.png'
 import tabs from '/header/tabs.svg'
 import phone from '/header/phone.svg'
 import styles from './index.module.scss'
-
+import { useRef, useLayoutEffect } from 'react'
+import gsap from 'gsap'
 export default function Header() {
+    // const comp = useRef(null); // create a ref for the root level element (we'll use it later)
+
+    // useLayoutEffect(() => {
+        
+    //     gsap.fromTo(comp.current,{
+    //         x:'-10rem'
+    //     },{
+    //         x:'2rem'
+    //     })
+    
+  
+  
+    // }, []); //
     return (
         <div className="text-center pt-16">
             <h1 className="font-main font-black text-black text-7xl ">Keep your plants alive</h1>
